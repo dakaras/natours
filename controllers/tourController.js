3b07ed7690b585beef0d67f8db9645bc23e91efa
dakaras <dan.karagiannis@protonmail.ch>
2020-07-29 13:36:08 -0500

    added route alias

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,12 @@
-const fs = require('fs')
+// const fs = require('fs')
 const Tour = require('./../models/tourModel')
 
+exports.aliasTopTours = (req, res, next) =>{
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+}
 
 exports.getAllTours = async (req, res) => {
     try {
@@ -12,21 +18,34 @@ exports.getAllTours = async (req, res) => {
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
         let query = Tour.find(JSON.parse(queryStr))
         
+        //sorting
         if(req.query.sort){
             const sortBy = req.query.sort.split(',').join(' ')
             query = query.sort(sortBy)
         } else {
             query = query.sort('-createdAt')
         }
-
+        //field limiting
         if(req.query.fields){
             const fields = req.query.fields.split(',').join(" ");
             query = query.select(fields)
         } else {
             query = query.select('-__v')
         }
+        //pagination
+        const page = req.query.page * 1  || 1 // string request object times one is a neat trick to convert the string into a number
+        const limit = req.query.limit * 1 || 100
+        const skip = (page - 1) * limit
+        //page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30, page 3
+        query = query.skip(skip).limit(limit)
+        if(req.query.page){
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error('This page does not exist.')
+        }
+        //execute query
         const tours = await query
         
+        //send response
         res.status(200).json({
             status: 'success',
             results: tours.length,
